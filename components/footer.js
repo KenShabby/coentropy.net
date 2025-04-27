2f2fa136bcec3066c5cb55c950393de10a328b6f
Brendan Coen <brendan@sonic.net>
2025-04-26 19:07:03 -0700

    Centered a div!

--- components/footer.js
+++ components/footer.js
@@ -7,6 +7,9 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <style>
+            footer p {
+                text-align: center;
+            }
             </style>
             <footer>
                 <hr>
