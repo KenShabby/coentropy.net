8ef8181999b180f2ab879917187bc8fdee706482
Ken Shabby <brendan@sonic.net>
2025-05-25 11:53:31 -0700

    deleted skeleton form handler

--- components/contact_form_handler.js
+++
@@ -1,7 +0,0 @@
-let form = document.forms.contact;
-
-let fname = form.elements.fname;
-let lname = form.elements.lname;
-let email = form.elements.email;
-let phone = form.elements.phone;
-let problem = form.elements.problem;
